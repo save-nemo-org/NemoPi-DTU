744bbd34edb70d8efd371587e646de6d6f845906
Han Liu <liuhan211211@gmail.com>
2024-07-14 10:27:19 +1000

    bugfix in fskv_set_credentials

--- src/utils.lua
+++ src/utils.lua
@@ -18,58 +18,12 @@ function utils.fskv_setup()
     end
 end
 
-function utils.fskv_get_cert_key()
-    local cert = fskv.get("cert")
-    if not cert then
-        log.error("fskv", "get", "cert", "not exist")
-        return false
-    end
-    local key = fskv.get("key")
-    if not cert then
-        log.error("fskv", "get", "key", "not exist")
-        return false
-    end
-    return true, cert, key
-end
-
-function utils.fskv_set_cert_key(cert, key)
-    local ret
-    if type(cert) ~= "string" then
-        log.error("fskv", "set", "wrong cert type", type(cert))
-        return false
-    end
-    if type(key) ~= "string" then
-        log.error("fskv", "set", "wrong cert type", type(key))
-        return false
-    end
-    ret = fskv.set("cert", cert)
-    if not ret then
-        log.error("fskv", "set", "failed to set cert")
-        return false
-    end
-    ret = fskv.set("key", key)
-    if not ret then
-        log.error("fskv", "set", "failed to set key")
-        return false
-    end
-    return true
-end
-
 function utils.fskv_set_credentials(credentials)
-    if type(credentials) ~= "table" then
-        log.error("fskv", "set", "wrong cert type", type(cert))
-        return false
-    end
-    if type(key) ~= "string" then
-        log.error("fskv", "set", "wrong cert type", type(key))
-        return false
-    end
-
     assert(type(credentials) == "table", "credentials should be a table")
-    assert(credentials.get("username") ~= nil, "missing username on setting credentials")
-    assert(credentials.get("password") ~= nil, "missing password on setting credentials")
-    assert(credentials.get("cert") ~= nil, "missing cert on setting credentials")
-    assert(credentials.get("key") ~= nil, "missing key on setting credentials")
+    assert(credentials["username"] ~= nil, "missing username on setting credentials")
+    assert(credentials["password"] ~= nil, "missing password on setting credentials")
+    assert(credentials["cert"] ~= nil, "missing cert on setting credentials")
+    assert(credentials["key"] ~= nil, "missing key on setting credentials")
 
     assert(fskv.set("credentials", credentials), "failed to set credentials")
     return true

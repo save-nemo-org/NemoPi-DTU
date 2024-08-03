bf422e26941a500ca06ee1d852e03ae9b7f78400
Han Liu <liuhan211211@gmail.com>
2024-08-03 20:08:24 +1000

    improve pm

--- src/main.lua
+++ src/main.lua
@@ -5,21 +5,17 @@ VERSION = "0.0.1"
 _G.sys = require("sys")
 _G.sysplus = require("sysplus")
 
--- log.setLevel(log.LOG_INFO)
-
 -- Disable power key debouncing
 if rtos.bsp() == "EC618" and pm and pm.PWK_MODE then
     pm.power(pm.PWK_MODE, false)
 end
 
 if rtos.bsp() == "EC618" and pm and pm.WORK_MODE then
+    log.setLevel(log.LOG_INFO)
     pm.power(pm.WORK_MODE, 1)
+    pm.power(pm.USB, false)
+    pm.request(pm.LIGHT)
 end
-
--- 自动低功耗, 轻休眠模式
--- Air780E支持uart唤醒和网络数据下发唤醒, 但需要断开USB,或者pm.power(pm.USB, false) 但这样也看不到日志了
--- Enable auto sleep
--- pm.request(pm.LIGHT)
 
 rtc.timezone(0)
 socket.setDNS(socket.LWIP_GP, 1, "8.8.8.8")
